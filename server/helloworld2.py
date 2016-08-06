a83829cc8298344d41d01a80fa298c0171d0326c
Haejong Dong <s2haejong@gmail.com>
2016-08-06 13:38:55 +0300

    clean compiled files

--- server/helloworld2.py
+++
@@ -1,9 +0,0 @@
-import config
-import logging as logger
-import json
-
-from server import api
-from flask import Blueprint, Flask, redirect, request, Response, url_for
-from .model_mongodb import MongoDB
-
-logger.info(" helloworld2 imported")
